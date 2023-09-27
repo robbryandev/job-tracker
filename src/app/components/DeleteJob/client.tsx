0f44b3e025ca2fc56cb3d56be24b91c4f3cbabbb
robbryandev <robbryandev@gmail.com>
2023-09-27 10:59:07 -0700

    Style delete confirmation

--- src/app/components/DeleteJob/client.tsx
+++ src/app/components/DeleteJob/client.tsx
@@ -14,15 +14,15 @@ export default function DeleteJob({ currentJob }: { currentJob: JobDb }) {
       </Popover.Trigger>
       <Popover.Portal>
         <Popover.Content>
-          <div>
+          <div className="bg-white shadow-md p-6">
             <form action={() => deleteJob(currentJob)} onSubmit={() => {
               setTimeout(() => {
                 router.push("/dashboard");
               }, 250)
             }}>
               <h2>Are you sure you want to delete this job?</h2>
-              <div>
-                <button type="submit">Delete</button>
+              <div className="pt-2 space-x-6">
+                <button type="submit" className="font-semibold underline underline-offset-2">Delete</button>
                 <Popover.Close>
                   Cancel
                 </Popover.Close>
